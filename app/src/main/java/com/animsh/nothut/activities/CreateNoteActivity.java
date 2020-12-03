8c7b7b1087f0e32d7b4d1b2e213e376c1838f084
animsh <animsh.more@gmail.com>
2020-12-03 16:45:00 +0530

    changelog: 1. Note Color

--- app/src/main/java/com/animsh/nothut/activities/CreateNoteActivity.java
+++ app/src/main/java/com/animsh/nothut/activities/CreateNoteActivity.java
@@ -2,11 +2,14 @@ package com.animsh.nothut.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.animsh.nothut.R;
 import com.animsh.nothut.database.NotesDatabase;
 import com.animsh.nothut.entities.Note;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,6 +28,8 @@ public class CreateNoteActivity extends AppCompatActivity {
 
     private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
     private TextView textDataTime;
+    private View subtitleIndicator;
+    private String selectedNoteColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         inputNoteSubtitle = findViewById(R.id.input_note_subtitle);
         inputNoteText = findViewById(R.id.input_note);
         textDataTime = findViewById(R.id.text_data_and_time);
+        subtitleIndicator = findViewById(R.id.view_subtitle_indicator);
 
         textDataTime.setText(
                 new SimpleDateFormat("EEE, MMM d, ''yy 'at' h:mm a", Locale.getDefault())
@@ -55,16 +62,19 @@ public class CreateNoteActivity extends AppCompatActivity {
                 saveNote();
             }
         });
+        selectedNoteColor = "#1F1F1F";
+        initMiscellaneous();
+        setSubtitleIndicatorColor();
     }
 
     private void saveNote() {
         if (inputNoteTitle.getText().toString().trim().isEmpty()) {
             Toast.makeText(CreateNoteActivity.this, "Note title can't be empty", Toast.LENGTH_SHORT).show();
             return;
-        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty()) {
+        } /*else if (inputNoteSubtitle.getText().toString().trim().isEmpty()) {
             Toast.makeText(CreateNoteActivity.this, "Note ub title can't be empty", Toast.LENGTH_SHORT).show();
             return;
-        } else if (inputNoteText.getText().toString().trim().isEmpty()) {
+        }*/ else if (inputNoteText.getText().toString().trim().isEmpty()) {
             Toast.makeText(CreateNoteActivity.this, "Note can't be empty", Toast.LENGTH_SHORT).show();
             return;
         }
@@ -74,7 +84,7 @@ public class CreateNoteActivity extends AppCompatActivity {
         note.setSubtitle(inputNoteSubtitle.getText().toString());
         note.setNoteText(inputNoteText.getText().toString());
         note.setDateTime(textDataTime.getText().toString());
-
+        note.setColor(selectedNoteColor);
         @SuppressLint("StaticFieldLeak")
         class SaveNoteTask extends AsyncTask<Void, Void, Void> {
 
@@ -94,5 +104,96 @@ public class CreateNoteActivity extends AppCompatActivity {
         }
 
         new SaveNoteTask().execute();
+    }
+
+    private void initMiscellaneous() {
+        final LinearLayout layoutMiscellaneous = findViewById(R.id.layout_miscellaneous);
+        final BottomSheetBehavior<LinearLayout> bottomSheetBehavior = BottomSheetBehavior.from(layoutMiscellaneous);
+        layoutMiscellaneous.findViewById(R.id.text_miscellaneous).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                } else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+        });
+        final ImageView imageColor1 = layoutMiscellaneous.findViewById(R.id.image_color_1);
+        final ImageView imageColor2 = layoutMiscellaneous.findViewById(R.id.image_color_2);
+        final ImageView imageColor3 = layoutMiscellaneous.findViewById(R.id.image_color_3);
+        final ImageView imageColor4 = layoutMiscellaneous.findViewById(R.id.image_color_4);
+        final ImageView imageColor5 = layoutMiscellaneous.findViewById(R.id.image_color_5);
+
+        layoutMiscellaneous.findViewById(R.id.view_color_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedNoteColor = "#1F1F1F";
+                imageColor1.setImageResource(R.drawable.ic_round_done);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setSubtitleIndicatorColor();
+            }
+        });
+
+        layoutMiscellaneous.findViewById(R.id.view_color_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedNoteColor = "#FDBE3B";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(R.drawable.ic_round_done);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setSubtitleIndicatorColor();
+            }
+        });
+
+        layoutMiscellaneous.findViewById(R.id.view_color_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedNoteColor = "#FF4842";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(R.drawable.ic_round_done);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setSubtitleIndicatorColor();
+            }
+        });
+
+        layoutMiscellaneous.findViewById(R.id.view_color_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedNoteColor = "#4285F4";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(R.drawable.ic_round_done);
+                imageColor5.setImageResource(0);
+                setSubtitleIndicatorColor();
+            }
+        });
+
+        layoutMiscellaneous.findViewById(R.id.view_color_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedNoteColor = "#000000";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(R.drawable.ic_round_done);
+                setSubtitleIndicatorColor();
+            }
+        });
+
+    }
+
+    private void setSubtitleIndicatorColor(){
+        GradientDrawable gradientDrawable = (GradientDrawable) subtitleIndicator.getBackground();
+        gradientDrawable.setColor(Color.parseColor(selectedNoteColor));
     }
 }
--- app/src/main/java/com/animsh/nothut/adapters/NoteAdapter.java
+++ app/src/main/java/com/animsh/nothut/adapters/NoteAdapter.java
@@ -1,8 +1,11 @@
 package com.animsh.nothut.adapters;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -50,12 +53,14 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
 
     static class NoteViewHolder extends RecyclerView.ViewHolder {
         TextView textTitle, textSubtitle, textDateTime;
+        LinearLayout layoutNote;
 
         public NoteViewHolder(@NonNull View itemView) {
             super(itemView);
             textTitle = itemView.findViewById(R.id.text_title);
             textSubtitle = itemView.findViewById(R.id.text_subtitle);
             textDateTime = itemView.findViewById(R.id.text_date_time);
+            layoutNote = itemView.findViewById(R.id.layout_note);
         }
 
         void setNote(Note note) {
@@ -66,6 +71,13 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
                 textSubtitle.setText(note.getSubtitle());
             }
             textDateTime.setText(note.getDateTime());
+
+            GradientDrawable gradientDrawable = (GradientDrawable) layoutNote.getBackground();
+            if (note.getColor() != null) {
+                gradientDrawable.setColor(Color.parseColor(note.getColor()));
+            } else {
+                gradientDrawable.setColor(Color.parseColor("#1F1F1F"));
+            }
         }
     }
 }
