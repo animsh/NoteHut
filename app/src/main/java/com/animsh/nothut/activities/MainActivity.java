2a46574fb5f5d76feb18396f521d84935036ba34
animsh <animsh.more@gmail.com>
2020-12-03 15:43:53 +0530

    changelog: 1. Display notes

--- app/src/main/java/com/animsh/nothut/activities/MainActivity.java
+++ app/src/main/java/com/animsh/nothut/activities/MainActivity.java
@@ -7,17 +7,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.animsh.nothut.R;
+import com.animsh.nothut.adapters.NoteAdapter;
 import com.animsh.nothut.database.NotesDatabase;
 import com.animsh.nothut.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +44,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        getNotes();
-    }
+        notesRecyclerView = findViewById(R.id.notes_recyclerview);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
 
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
+        noteList = new ArrayList<>();
+        noteAdapter = new NoteAdapter(noteList);
+        notesRecyclerView.setAdapter(noteAdapter);
         getNotes();
     }
 
@@ -57,9 +68,25 @@ public class MainActivity extends AppCompatActivity {
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
                 Log.d("MY_NOTES: ", notes.toString());
+                if (noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    noteAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    noteAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
 
         new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
     }
 }
---
+++ app/src/main/java/com/animsh/nothut/adapters/NoteAdapter.java
@@ -0,0 +1,71 @@
+package com.animsh.nothut.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.animsh.nothut.R;
+import com.animsh.nothut.entities.Note;
+
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private List<Note> notes;
+
+    public NoteAdapter(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.item_container_note,
+                        parent,
+                        false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.setNote(notes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView textTitle, textSubtitle, textDateTime;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textTitle = itemView.findViewById(R.id.text_title);
+            textSubtitle = itemView.findViewById(R.id.text_subtitle);
+            textDateTime = itemView.findViewById(R.id.text_date_time);
+        }
+
+        void setNote(Note note) {
+            textTitle.setText(note.getTitle());
+            if (note.getSubtitle().trim().isEmpty()) {
+                textSubtitle.setVisibility(View.GONE);
+            } else {
+                textSubtitle.setText(note.getSubtitle());
+            }
+            textDateTime.setText(note.getDateTime());
+        }
+    }
+}
